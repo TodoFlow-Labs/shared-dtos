e87c4057bac9d39f2a527d4d5f9db6fba4b96b5d
lieranderl <19622412+lieranderl@users.noreply.github.com>
2025-05-10 15:58:31 +0200

    test metric

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -3,6 +3,7 @@ package metrics
 import (
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog/log"
 )
@@ -11,8 +12,19 @@ func Init(addr string) {
 	http.Handle("/metrics", promhttp.Handler())
 	go func() {
 		log.Info().Msgf("metrics server listening on %s", addr)
+		// Start the HTTP server for metric
 		if err := http.ListenAndServe(addr, nil); err != nil {
 			log.Error().Err(err).Msg("metrics server failed")
 		}
 	}()
+	log.Info().Msgf("metrics server listening on %s", addr)
+	log.Info().Msg("metrics server started")
 }
+
+var TodoCreateCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "todos_created_total",
+		Help: "Total number of todos created, labeled by status",
+	},
+	[]string{"status"},
+)
